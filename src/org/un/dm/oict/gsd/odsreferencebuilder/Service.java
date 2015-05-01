b4eecdf74419a9bac39f2b3f24d328a6ba2ab6bc
Kevin Bradley <kevinbradley@work.local>
2015-05-01 16:32:44 -0400

    Added comment standards to the Service class

--- src/org/un/dm/oict/gsd/odsreferencebuilder/Service.java
+++ src/org/un/dm/oict/gsd/odsreferencebuilder/Service.java
@@ -13,27 +13,56 @@ import java.io.File;
 */
 public class Service {
 	
+	static String xmlFileDirectory = "/Users/kevinbradley/Desktop/testFiles";
+	static String solrInstance = "";
+	static String solrCollection = "";
+	static String solrUsername = "";
+	static String solrPassword = "";
+	static String databaseLocation = "";
+	static String databaseName = "";
+	static String databaseUsername = "";
+	static String databasePassword = "";
+	static String tikaTesseractServer = "http://frankie:9998/tika";
+	
+	/**
+	 * Entry point to application
+	 * @param args
+	 */
 	public static void main(String args[]) {
-		performTask("/Users/kevinbradley/Desktop/testFiles");
+		performTask(xmlFileDirectory);
 	}
 	
+	/**
+	 * Method which performs the looping of a given directory
+	 * to obtain the relevant xml files, from this it processes each
+	 * and writes the output to an individual file
+	 * @param xmlFilesDirectory
+	 */
 	static void performTask(String xmlFilesDirectory) {
 		
+		// Set file directory for XML files
 		File dir = new File(xmlFilesDirectory);
 		File[] directoryListing = dir.listFiles();
+		// Verify not null
 		if (directoryListing != null) {
+			// Loop each file within the directory
 			for (File child : directoryListing) {
+				// Get current filename
 				String currXmlFilename = child.getAbsolutePath();
+				// Verify its an XML file, that is all we are interested in
 				if (currXmlFilename.endsWith(".xml")) {
+					
+					// Instantiate two new SolrDocuments and one Reference Document
 					SolrDocument currentSolrDocument = TextExtractorFile.readXmlDocument(currXmlFilename);
 					SolrDocument newSolrDocument = new SolrDocument();
 					ReferenceDocument newReferenceDocument = new ReferenceDocument();
 
 					// Process the Solr Document - Cleanse & apply business logic
+					// If required obtain a new body
 					newSolrDocument = FileProcessor.processFile(currentSolrDocument, newSolrDocument);
 	
-					// Extract references for all Symbol variations - to obtain that list
-					// TODO Further refinement of the regex and output
+					// Extract references for all Symbol variations
+					// TODO Test references, there seems to be a lost of noise, code below shows output
 					newReferenceDocument = FileProcessor.extractReferences(newSolrDocument, newReferenceDocument);
 					/*String refs = "[";
 					for(String s : newReferenceDocument.getReferences()) {
@@ -42,9 +71,9 @@ public class Service {
 					refs += "]";
 					System.out.println("Id : " + newReferenceDocument.getId() + " refs: " + refs);*/
 	
-					// Write out new XML Solr File
+					// Write out newly created Solr Document
 					/*OutputFile.writeSolrDocument("filename", newSolrDocument.toString());
-					// Write out new XML Reference File
+					// Write out newly created Reference Document
 					OutputFile.writeReferenceDocument("filename", newReferenceDocument.toString());
 					// Store in Database
 					OutputDatabase.logData();
