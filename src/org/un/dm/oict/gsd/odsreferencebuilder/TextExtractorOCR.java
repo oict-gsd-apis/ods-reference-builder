71ac1684cfe3e7c44145504536a76f1fb635f44f
Daniel Buenavad <buenavad@gmail.com>
2015-05-05 11:28:09 -0400

    OCR UPDATE

--- src/org/un/dm/oict/gsd/odsreferencebuilder/TextExtractorOCR.java
+++ src/org/un/dm/oict/gsd/odsreferencebuilder/TextExtractorOCR.java
@@ -1,9 +1,16 @@
 package org.un.dm.oict.gsd.odsreferencebuilder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 /**
  * @author Kevin Thomas Bradley
@@ -50,6 +57,10 @@ public class TextExtractorOCR {
 	            			"http://frankie:9998/tika",
 	            			"--header",
 	            			"\"Accept: text/plain\""};
+		return performProcess(command);
+	}
+	
+	static String performProcess(String[] command) {
 		// Create a new ProcessBuilder which will run the command
         ProcessBuilder probuilder = new ProcessBuilder( command );
         
@@ -72,7 +83,7 @@ public class TextExtractorOCR {
 			while ((line = br.readLine()) != null) {
 				// Remove excess white space
 				if (line.length() > 0)
-					body += line.replaceAll("[\n\r]","") + "\n";
+					body += minimizeWhitespace(line);
 			}
 		} catch (IOException e) {
 			return "";
@@ -85,5 +96,65 @@ public class TextExtractorOCR {
         	return "";
         }
         return body;
+	}
+	
+	static String minimizeWhitespace(String val) {
+		return val.replaceAll("[\n\r]","") + "\n";
+	}
+	
+	static String performCompleteOCR(String tempOutputDir, String pdfUrl, String languageCode) {
+		String tempId = UUID.randomUUID().toString();
+		String imgFile = tempOutputDir + "" + tempId + ".tif";
+		String txtFile = tempOutputDir + "" + tempId + "";
+		String lang = mapLanguageCode(languageCode);
+		// TODO Copy PDF locally
+		// Convert pdf to images in a specific folder
+		String[] command = { "gs",
+        			"-dNOPAUSE",
+        			"-sDEVICE=tiffg4",
+        			"-r600x600",
+        			"-dBATCH",
+        			"-sPAPERSIZE=a4",
+        			"-sOutputFile=" + imgFile,
+        			pdfUrl};
+		performProcess(command);
+		// Use Tesseract to convert these images to text based on the languageCode
+		String[] command2 = { "tesseract",
+				imgFile,
+				txtFile,
+    			"-l",
+    			lang};
+		performProcess(command2);
+		
+		// Read the contents of this file and store in a variable
+		String body = readFile(txtFile + ".txt", StandardCharsets.UTF_8);
+		
+		try {
+			Files.delete(Paths.get(imgFile)); 
+			Files.delete(Paths.get(txtFile));
+		} catch (IOException e) {
+
+		} 
+		return body;
+	}
+	
+	static String readFile(String path, Charset encoding) {
+		byte[] encoded = null;
+		try {
+			encoded = Files.readAllBytes(Paths.get(path));
+		} catch (IOException e) {
+			
+		}
+		return new String(encoded, encoding);
+	}
+
+	static String mapLanguageCode(String languageCode) {
+		// en-> eng
+		if (languageCode.equals("en"))
+			return "eng";
+		else if (languageCode.equals("ar"))
+			return "ara";
+		else
+			return "eng";
 	}
 }
